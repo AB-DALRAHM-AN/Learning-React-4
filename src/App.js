106657b1587e04df6c73f6707dbbadea7636ae6e
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-10-12 21:16:19 +0300

    set, use  state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react';
+import Example from './components/count';
+import Counter from './components/countClass';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Example />
+      <Counter />
     </div>
   );
 }
